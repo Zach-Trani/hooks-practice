f9a1dad2840e80781419d4805a4efddd2073c5a6
Zach-Trani <trani.zach@gmail.com>
2022-12-09 20:13:47 -0500

    destructuring onTitleClick for organizational purposes

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -2,16 +2,22 @@ import React from "react";
 
 // only Accrodion will know which question is expanded: activeIndex
 
+// helper method outside the body of the function component helps with organization.
 const Accordion = ({ items }) => {
+    const onTitleClick = (index) => {
+        console.log('title clicked', index);
+    };
+
     const renderedItems = items.map((item, index) => {
         return (
+            // semantic ui places an extra border when we use a div here, therefore Rect.Fragment is used.
             <React.Fragment key={item.title}>
                 <div
                     className="title active"
-                    // empty () will prevent the immediate console log.
-                    onClick={() => console.log('Title Clicked', index)}>
-
-
+                    // console log here and receive the item index.
+                    // wrapping with an arrow function invokes a function when onClick is clicked, otherwise it runs on the browser loading.
+                    onClick={() => onTitleClick(index)}
+                >
                     <i className="dropdown icon"></i>
                     {item.title}
                 </div>
