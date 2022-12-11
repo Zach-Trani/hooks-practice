6404028ef7d2c1f6cb7d02f5f528d52c6ba950d1
Zach-Trani <trani.zach@gmail.com>
2022-12-11 16:35:01 -0500

    finished accordion component complete with drop down style icons that make use of useState

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 
-// only Accrodion will know which question is expanded: activeIndex
+// only Accordion will know which question is expanded: activeIndex
 
 // helper method outside the body of the function component helps with organization.
 const Accordion = ({ items }) => {
     // array destructuring
-    // [piece-of-state, function-to-change-this-piece-of-state] = useState(initial-value-for-this-piece-of-state)
+    // [piece-of-state, function-to-change-this-piece-of-state] = useState(initial-value-for-this-piece-of-state) <-- standard across the state hook
+    // could be a counter: const [counter, setCounter] = useState(0);
     const [activeIndex, setActiveIndex] = useState(null);
     // setter function updates state & causes component to rerender
     const onTitleClick = (index) => {
@@ -13,11 +14,13 @@ const Accordion = ({ items }) => {
     };
 
     const renderedItems = items.map((item, index) => {
+        const active = index === activeIndex ? 'active' : '';
+
         return (
-            // semantic ui places an extra border when we use a div here, therefore Rect.Fragment is used.
+            // semantic ui places an extra border when we use a div here, therefore React.Fragment is used.
             <React.Fragment key={item.title}>
                 <div
-                    className="title active"
+                    className={`title ${active}`}
                     // console log here and receive the item index.
                     // wrapping with an arrow function invokes a function when onClick is clicked, otherwise it runs on the browser loading.
                     onClick={() => onTitleClick(index)}
@@ -25,7 +28,7 @@ const Accordion = ({ items }) => {
                     <i className="dropdown icon"></i>
                     {item.title}
                 </div>
-                <div className="content active">
+                <div className={`content ${active}`}>
                     <p>{item.content}</p>
                 </div>
             </React.Fragment>
@@ -34,7 +37,6 @@ const Accordion = ({ items }) => {
 
     return <div className="ui styled accordion">
         {renderedItems}
-        <h1>{activeIndex}</h1>
     </div>
 };
 
